74ddc73f9193f671e8ea8e25259dbd0e474de6f0
bettk6516 <bettk6516@HRH-IC0027345.wrdsb.ca>
2015-11-24 14:19:47 -0500

    Quiz 6

---
+++ src/quiz5.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bettk6516
+ */
+public class quiz5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+        Scanner input = new Scanner(System.in);
+        while(true){
+        System.out.println("Please enter the word you would like to translate form american to canadian");
+    
+    String word =input.nextLine();
+    
+    if(word == "quit!"){
+        break;
+    }
+   
+    int wordLenth = word.length();
+    if(wordLenth > 4){
+         
+        if(lastIndexOf(string "or") > indexOf("a")){
+        
+    }
+    }
+    
+    
+        }
+    }
+}
